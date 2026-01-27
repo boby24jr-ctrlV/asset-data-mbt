d9f79d4480bf880c53ff669ed0f5c12f1d1f97fe
alifrizky <alif.mrizky2007@gmail.com>
2026-01-27 15:16:37 +0700

    init laravel + dashboard template

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>SIMAR | Sistem Informasi Maintenance & Repair</title>
+
+    {{-- Dashboard CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/dashboard-layout.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/charts-layout.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/widgets.css') }}">
+</head>
+<body>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">SIMAR</a>
+
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tracking</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
